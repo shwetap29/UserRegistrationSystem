475ae695e2c180a6d8107cca9a4bbad99bee67c4
shwetap29 <shwetaprasad48@gmail.com>
2021-05-27 07:48:39 +0530

    ADDED: Validator For First Name

--- src/main/java/com/bridgelabz/UserMain.java
+++ src/main/java/com/bridgelabz/UserMain.java
@@ -1,7 +1,26 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserMain {
+    public static boolean firstNameValidation(String FirstName){
+        String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        if (FirstName == null){
+            return false;
+        }
+        Matcher m = p.matcher(FirstName);
+        return m.matches();
+
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Registration System");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter firstname: ");
+        String FirstName = input.nextLine();
+        System.out.println(firstNameValidation(FirstName));
     }
 }
