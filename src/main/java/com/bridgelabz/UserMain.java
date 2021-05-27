ae7b8733535adf7ea62e94b8ba3e838e379ead38
shwetap29 <shwetaprasad48@gmail.com>
2021-05-27 15:14:33 +0530

    Added:Validator For Email

--- src/main/java/com/bridgelabz/UserMain.java
+++ src/main/java/com/bridgelabz/UserMain.java
@@ -16,6 +16,28 @@ public class UserMain {
 
     }
 
+      public static boolean lastNameValidation(String LastName){
+        String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        if (LastName == null){
+            return false;
+        }
+        Matcher m = p.matcher(LastName);
+        return m.matches();
+
+    }
+
+    public static boolean emailValidation(String Email){
+        String regexEmail = "^[0-9 A-Z a-z]+(([._+-]*)[0-9A-Za-z]+)*@[0-9 A-Z a-z]+.[a-z]{2,4}([.][a-z]{2,3})*$";
+        Pattern p = Pattern.compile(regexEmail);
+        if (Email == null) {
+            return false;
+        }
+        Matcher m = p.matcher(Email);
+        return  m.matches();
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Registration System");
         Scanner input = new Scanner(System.in);
@@ -26,7 +48,12 @@ public class UserMain {
 
         System.out.println("Please Enter your lastname: ");
         String LastName = input.nextLine();
-        System.out.println(firstNameValidation(LastName));
+        System.out.println(lastNameValidation(LastName));
+
+        System.out.println("Please Enter your Email: ");
+        String EmailID = input.nextLine();
+        System.out.println(emailValidation(EmailID));
+
 
     }
 }
