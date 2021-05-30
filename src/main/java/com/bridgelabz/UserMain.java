10a5363771df05a60a51d01e104eb2f79c7d0417
shwetap29 <shwetaprasad48@gmail.com>
2021-05-30 19:56:46 +0530

    REFACTORED: Added Email Samples in email validation

--- src/main/java/com/bridgelabz/UserMain.java
+++ src/main/java/com/bridgelabz/UserMain.java
@@ -1,19 +1,20 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserMain {
+
     public static boolean firstNameValidation(String FirstName){
         String regexFirstName = "^[A-Z][a-z A-Z]{2,}$";
         Pattern p = Pattern.compile(regexFirstName);
-        if (FirstName == null){
+        if (FirstName == null) {
             return false;
         }
         Matcher m = p.matcher(FirstName);
         return m.matches();
-
     }
 
       public static boolean lastNameValidation(String LastName){
@@ -26,15 +27,41 @@ public class UserMain {
         return m.matches();
 
     }
-    public static boolean emailValidation(String Email){
+    public static void emailValidation() {
+        ArrayList<String> emails = new ArrayList<String>();
+        emails.add("abc@yahoo.com");
+        emails.add("abc-100@yahoo.com");
+        emails.add("abc.100@yahoo.com");
+        emails.add("abc111@abc.com");
+        emails.add("abc-100@abc.net");
+        emails.add("abc.100@abc.com.au");
+        emails.add("abc@1.com");
+        emails.add("abc@gmail.com");
+        emails.add("abc@gmail.com.com");
+        emails.add("abc+100@gmail.com");
+
+        emails.add("abc");
+        emails.add("abc@.com.my");
+        emails.add("abc123@gmail.a");
+        emails.add("abc123@.com");
+        emails.add("abc123@.com.com");
+        emails.add(".abc@abc.com");
+        emails.add("abc()*@gmail.com");
+        emails.add("abc@%*.com");
+        emails.add("abc..2002@gmail.com");
+        emails.add("abc.@gmail.com");
+        emails.add("abc@abc@gmail.com");
+        emails.add("abc@gmail.com.1a");
+        emails.add("abc@gmail.com.aa.au");
+
         String regexEmail = "^[0-9 A-Z a-z]+(([._+-]*)[0-9A-Za-z]+)*@[0-9 A-Z a-z]+.[a-z]{2,4}([.][a-z]{2,3})*$";
         Pattern p = Pattern.compile(regexEmail);
-        if (Email == null) {
-            return false;
-        }
-        Matcher m = p.matcher(Email);
-        return  m.matches();
 
+        for (String Email : emails) {
+            //Create instance of matcher
+            Matcher matcher = p.matcher(Email);
+            System.out.println(emails + " : " + matcher.matches() + "\n");
+        }
     }
     public static boolean mobileNumValidation(String MobileNum ){
         String regexMobileNum = "^[9][1]\\s[6-9][0-9]{9}$";
@@ -69,9 +96,8 @@ public class UserMain {
         String LastName = input.nextLine();
         System.out.println(lastNameValidation(LastName));
 
-        System.out.println("Please Enter your Email: ");
-        String EmailID = input.nextLine();
-        System.out.println(emailValidation(EmailID));
+        System.out.println("Lets Validate Email Address ");
+        emailValidation();
 
         System.out.println("Please Enter the mobile number with country code 91: ");
         String MobileNum = input.nextLine();
@@ -81,4 +107,5 @@ public class UserMain {
       String Password = input.nextLine();
       System.out.println(PasswordValidation(Password));
     }
-}
+    }
+
